c1c60f5d3caec7728c59807997a6044ff5a916ca
Toshinari Itoko <itoko@jp.ibm.com>
2020-05-27 11:28:04 +0900

    temporal work-around

--- qiskit/scheduler/methods/lowering.py
+++ qiskit/scheduler/methods/lowering.py
@@ -91,7 +91,8 @@ def lower_gates(circuit: QuantumCircuit, schedule_config: ScheduleConfig) -> Lis
                 duration = int(duration_s // schedule_config.dt)
             sched = Schedule(name=inst.name)
             for qubit in inst_qubits:
-                for channel in [DriveChannel, AcquireChannel, MeasureChannel]:
+                # for channel in [DriveChannel, AcquireChannel, MeasureChannel]:
+                for channel in [DriveChannel]:
                     sched += pulse_inst.Delay(duration=duration, channel=channel(qubit))
             circ_pulse_defs.append(CircuitPulseDef(schedule=sched, qubits=inst_qubits))
 
